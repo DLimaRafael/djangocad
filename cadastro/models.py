aa456ce6b165c2af81b3d3ef4edb3dfb696af806
Rafael Dias <persephius@protonmail.com>
2022-10-14 11:48:20 -0300

    Adjusting URLs and writing HTML templates

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    usuario = models.TextField(blank=False)
+    senha = models.TextField(blank=True)
+    nasc = models.DateField(blank=False)
+
+    def __str__(self) -> str:
+        return super().__str__()
